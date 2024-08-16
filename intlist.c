f1155c5d1145b9bea2d3f5359aa2548e5f10a578
Ayisha <Ayisha.Ahamed@trumpf.com>
2024-08-16 11:47:23 +0530

    Assignment 2

--- intlist.c
+++ intlist.c
@@ -19,7 +19,7 @@ struct node {
 //Defines head pointer and fixes it to null
 struct node* Create () {
    struct node* head = malloc (sizeof (struct node));
-   printf ("An empty linked list is created \n");
+   printf ("\nAn empty linked list is created \n");
    head = NULL;
    return head;
 }
@@ -29,13 +29,12 @@ struct node* Create () {
 void PrintFunc (struct node* s) {
    struct node* temp = s;
    if (temp != NULL) {
-      printf ("The list elements are : \n");
+      printf ("\nThe list elements are : \n");
       while (temp != NULL) {
          printf ("%d -> ", temp->value);
          temp = temp->link;
       }
-      printf ("\n");
-   } else { printf ("No elements in the list\n"); }
+   } else { printf ("\nNo elements in the list\n"); }
 }
 
 // ------------------------------------------------------------------------------------------------
@@ -53,7 +52,7 @@ void Add (struct node** head, int val) {
    result->link = *head; //current struct head pointer is linked to the new element
    *head = result;// struct head pointer now points to the added element 
    struct node* store = *head;
-   printf ("%d is added to the list\n", (*head)->value);
+   printf ("\n%d is added to the list\n", (*head)->value);
    PrintFunc (store);
    return;
 }
@@ -63,20 +62,20 @@ void Add (struct node** head, int val) {
 void Delete (struct node** head) {
    struct node* store = *head;
    if (store == NULL) {
-      printf ("There are no elements to delete \n");
+      printf ("\nThere are no elements to delete \n");
       return;
    } else {
       struct node* temp;
       struct node* temp2;
       temp = store;
       while (temp != NULL) {
-         printf ("%d is deleted from the list\n", temp->value);
+         printf ("\n%d is deleted from the list\n", temp->value);
          temp2 = temp->link;
          free (temp);
          temp = NULL;
          temp = temp2;
       }
-      printf ("List deleted... \n");
+      printf ("\nList deleted... \n");
       *head = NULL;
       return;
    }
@@ -91,7 +90,7 @@ void Count (struct node** head) {
       store = store->link;
       count++;
    }
-   printf ("The number of elements in the list is %d\n", count);
+   printf ("\nThe number of elements in the list is %d\n", count);
 }
 
 // ------------------------------------------------------------------------------------------------
@@ -115,14 +114,14 @@ void RemoveAt (struct node** head, int n) {
    // to the required zero based index position  
    int cmp = lengthOfList - n - 1;
    if (*head == NULL) {
-      printf ("The list is empty \n");
+      printf ("\nThe list is empty \n");
       return;
    }
    if (n < 0) {
-      printf ("Invalid index \n");
+      printf ("\nInvalid index \n");
    }
    if (n > (lengthOfList - 1)) {
-      printf ("The index exceeds the length of the list (zero based) \n");
+      printf ("\nThe index exceeds the length of the list (zero based) \n");
       return;
    }
 //To remove the first element of the list
@@ -141,7 +140,7 @@ void RemoveAt (struct node** head, int n) {
          prev = temp;
          temp = temp->link;
       }
-      printf ("%d is removed from the list\n", temp->value);
+      printf ("\n%d is removed from the list\n", temp->value);
       prev->link = NULL;
       free (temp);
       PrintFunc (*head);
@@ -151,7 +150,7 @@ void RemoveAt (struct node** head, int n) {
    if (n == (lengthOfList - 1)) {
       struct node* temp;
       temp = *head;
-      printf ("%d is removed from the list\n", temp->value);
+      printf ("\n%d is removed from the list\n", temp->value);
       *head = (*head)->link;
       PrintFunc (*head);
       free (temp);
@@ -174,7 +173,7 @@ void RemoveAt (struct node** head, int n) {
       tempNext = temp->link;
    }
    prev->link = tempNext;
-   printf ("%d is removed from the list\n", temp->value);
+   printf ("\n%d is removed from the list\n", temp->value);
    free (temp);
    PrintFunc (*head);
 }
@@ -186,18 +185,18 @@ void Get (struct node** head, int n) {
    int cmp = lengthOfTheList - n - 1;
    struct node* temp = *head;
    if (n < 0) {
-      printf ("Invalid index , did you mean n = 0? \n");
+      printf ("\nInvalid index , did you mean n = 0? \n");
       return;
    }
    if (n > (lengthOfTheList - 1)) {
-      printf ("Exceeded length of the list \n");
+      printf ("\nExceeded length of the list \n");
       return;
    }
    while ((count < cmp) && (temp != NULL)) {
       temp = temp->link;
       count++;
    }
-   printf ("The element at index position %d is %d \n", n, temp->value);
+   printf ("\nThe element at index position %d is %d \n", n, temp->value);
 }
 // ------------------------------------------------------------------------------------------------
 //Removes the first occurence of an element from the list
@@ -205,7 +204,7 @@ void Remove (struct node** head, int n) {
    int count = (CountNum (head)) - 1;
    int indexPosition = -1;
    if (*head == NULL) {
-      printf ("There are no elements to remove \n");
+      printf ("\nThere are no elements to remove \n");
       return;
    } else {
       struct node* temp;
@@ -218,7 +217,7 @@ void Remove (struct node** head, int n) {
          count--;
       }
       if (indexPosition != -1) {
-         printf ("The earliest occurence of %d is in index position %d \n", n, indexPosition);
+         printf ("\nThe earliest occurence of %d is in index position %d \n", n, indexPosition);
          RemoveAt (head, indexPosition);
          return;
       } else {
@@ -238,14 +237,14 @@ void Insert (struct node** head, int n, int pos) {
    newNode->value = n;
    newNode->link = NULL;
    if ((pos > count) || (pos < 0)) {
-      printf ("Invalid insertion \n");
+      printf ("\nInvalid insertion \n");
       return;
    }
    //For inserting an element at the end of the list
    if (pos == count) {
       newNode->link = *head;
       *head = newNode;
-      printf ("%d is inserted at index position %d\n", (*head)->value, pos);
+      printf ("\n%d is inserted at index position %d\n", (*head)->value, pos);
       PrintFunc (*head);
       return;
    }
@@ -255,7 +254,7 @@ void Insert (struct node** head, int n, int pos) {
          current = current->link;
       }
       current->link = newNode;
-      printf ("%d is inserted at index position %d\n", n, pos);
+      printf ("\n%d is inserted at index position %d\n", n, pos);
       PrintFunc (*head);
       return;
    }
@@ -263,11 +262,9 @@ void Insert (struct node** head, int n, int pos) {
       prev = current;
       current = current->link;
    }
-   printf ("Prev value = %d\n", prev->value);
-   printf ("Current value = %d\n", current->value);
    prev->link = newNode;
    newNode->link = current;
-   printf ("%d is inserted at index position %d\n", n, pos);
+   printf ("\n%d is inserted at index position %d\n", n, pos);
    PrintFunc (*head);
    return;
 }
