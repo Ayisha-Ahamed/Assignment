c06a6d8ac0290487fa1353268461422e706a7cec
Ayisha <Ayisha.Ahamed@trumpf.com>
2024-08-30 18:18:26 +0530

    A3: Update

--- conversion.c
+++ conversion.c
@@ -15,24 +15,29 @@
 #define ERROR_MEM_ALLOC_FAILURE -1
 #define ERROR_EXCEEDED_LENGTH -2
 #define ERROR_INVALID_INPUT -3
+#define UNSIGNED_MAX 4294967295
 
-char Arr[16] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };    // Array of Hexadecimal equivalent characters
+char Arr[16] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };    // Array of hexadecimal equivalent characters
 
-/// <summary> Returns the nearest byte length </summary>
+char* StoreBin;      // Global variable that stores the binary string output
+int Length;
+/// <summary>Returns the nearest byte length</summary>
 int NearestByte (int digit, bool isLargestNum) {
 	int bitLen = 8;
 	for (int i = 1; i <= digit; i++) {
 		if (bitLen >= digit) return bitLen;
-		bitLen *= 2;              // Bit Lengths can be 8,16 or 32   
+		bitLen *= 2;              // Bit lengths can be 8,16 or 32   
 	}
-	if (isLargestNum == true && digit != 0)return bitLen * 2;
+	if (isLargestNum == true && digit != 0)bitLen *= 2;
+	Length = bitLen;
 	return bitLen;
 }
 
-/// <summary> Returns the binary equivalent of input to string </summary>
-char* BinaryString (int len, unsigned num) {
+/// <summary>Returns the binary equivalent of input to string</summary>
+char* BinaryString (int len, unsigned long num) {
+	if (num > UNSIGNED_MAX)return NULL;
 	int rLen = 0, bit = 0, sIndex = len - 1;
-	char* revBinStr = malloc (sizeof (char) * (len + 1));     // Stores the binary string starting from LSB to MSB
+	char* revBinStr = malloc (sizeof (char) * (len + 1));       // Stores the binary string starting from LSB to MSB
 	if (revBinStr == NULL) exit (ERROR_MEM_ALLOC_FAILURE);
 	for (; rLen < len; rLen++) revBinStr[rLen] = '0';           // Fills all the elements of the array to '0'
 	revBinStr[rLen] = '\0';
@@ -41,8 +46,8 @@ char* BinaryString (int len, unsigned num) {
 		if (bit == 1) revBinStr[i] = '1';
 		num /= 2;
 	}
-	char* binStr = malloc (sizeof (char) * (len + 1));             // Stores the binary string from MSB to LSB
-	if (binStr == NULL) exit (ERROR_MEM_ALLOC_FAILURE);
+	char* binStr = malloc (sizeof (char) * (len + 1));          // Stores the binary string from MSB to LSB
+	if (binStr == NULL || StoreBin == NULL) exit (ERROR_MEM_ALLOC_FAILURE);
 	binStr[len] = '\0';
 	for (int i = 0; i < len; i++) {
 		binStr[i] = revBinStr[sIndex];
@@ -52,36 +57,29 @@ char* BinaryString (int len, unsigned num) {
 	return binStr;
 }
 
-/// <summary> Converts the given input to binary </summary>
-char* Binary (int input) {
+/// <summary>Converts the given input to binary</summary>
+char* Binary (long int input) {
 	int digit;
-	long int power;
 	bool largestNum = 0;                // Flag sets if the number is the largest within the given byte length ex. 256,-255
-	unsigned num = (unsigned)input,numCpy = num;
+	unsigned long num = (unsigned long)input, numCpy = num;
 	for (digit = 0; numCpy > 0; digit++) numCpy /= 2;
 	if (digit > 32) return NULL;
-	power = pow (digit, 2);
-	if (num == (power - 1)) {
-		largestNum = 1;
-	}
+	if (num == (pow (digit, 2) - 1)) largestNum = 1;
 	char* binStr = BinaryString (NearestByte (digit, largestNum), num);
 	if (binStr == NULL)exit (ERROR_MEM_ALLOC_FAILURE);
 	return binStr;
 }
 
-/// <summary> Converts the given input to hexadecimal </summary>
-char* Hexadecimal (int num) {
+/// <summary>Converts the given input to hexadecimal</summary>
+char* Hexadecimal (char* StoreBin) {
 	int binLen = 0, hexLen = 0, length = 0, sum = 0;
-	char* storeBin = Binary (num);                  // Stores the binary value of the string returned
-	if (storeBin == NULL) return NULL;
-	for (int i = 0; storeBin[i] != '\0'; i++)length++;
+	if (StoreBin == NULL) return NULL;
+	for (int i = 0; StoreBin[i] != '\0'; i++)length++;
 	char* hexStr = malloc (sizeof (char) * ((length / 4) + 1));
 	if (hexStr == NULL)exit (ERROR_MEM_ALLOC_FAILURE);
 	while (binLen < length && hexLen < (length / 4)) {
 		sum = 0;
-		for (int i = 3; i >= 0; i--, binLen++) {
-			sum += (storeBin[binLen] - '0') * pow (2, i);
-		}
+		for (int i = 3; i >= 0; i--, binLen++) sum += (StoreBin[binLen] - '0') * (int)pow (2, i);
 		hexStr[hexLen] = Arr[sum];
 		hexLen++;
 	}
@@ -89,22 +87,27 @@ char* Hexadecimal (int num) {
 	return hexStr;
 }
 
-/// <summary> Method for printing hexadecimal characters using type specifier </summary>
-void Hex (int num) {
-	printf ("HEX : %X\n", num);
+/// <summary>Method for printing hexadecimal characters using type specifier</summary>
+void Hex (long int num) {
+	printf ("HEX : %lX\n", num);
 }
 
-/// <summary> Prints the output of the program </summary>
-int Call (int num, char* bin, char* hex) {
-	if (bin == NULL) {
+/// <summary>Prints the output of the program</summary>
+int Call (long long int input) {
+	StoreBin = malloc (sizeof (char) * (Length + 1));
+	if (input > 2147483647 || input < -2147483647) {
 		printf ("The input exceeds the maximum length (32 bit)\n");
-		return;
+		return ERROR_EXCEEDED_LENGTH;
 	}
-	printf ("INPUT : %d\n", num);
-	printf ("BIN : %s\n", bin);
-	printf ("HEX : %s \n", hex);
-	printf ("\n");
-	free (bin);
+	long int num = (long int)input;
+	StoreBin = Binary (num);
+	char* hex = Hexadecimal (StoreBin);
+	if (StoreBin == NULL) {
+		printf ("The input exceeds the maximum length (32 bit)\n");
+		return ERROR_EXCEEDED_LENGTH;
+	}
+	printf ("Input : %ld \nBIN : %s\nHEX : %s\n", num, StoreBin, hex);
+	free (StoreBin);
 	free (hex);
 	return 0;
 }
@@ -129,9 +132,9 @@ int main () {
 		}
 	}
 	inStr[lenIn] = '\0';
-	int num = atoi (inStr);
+	long long int num = atoll (inStr);
 	free (inStr);
-	if (num != 0) Call (num, Binary (num), Hexadecimal (num));
-	else Call (0, "0000", "0000");            // If the input is 0
+	if (num != 0) Call (num);
+	else printf ("Input : 0 \nBIN : 00000000\nHEX : 0000\n");            // If the input is 0
 	return 0;
 }
