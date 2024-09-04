b078be42d88bb69503648e780d6e595aed0482e6
Ayisha <Ayisha.Ahamed@trumpf.com>
2024-09-04 10:25:26 +0530

    A3: Number Conversion Game

--- conversion.c
+++ conversion.c
@@ -15,14 +15,15 @@
 #define ERROR_EXCEEDED_LENGTH -2
 #define ERROR_INVALID_INPUT -3
 
+
 char Arr[16] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };    // Array of hexadecimal equivalent characters
 
 /// <summary>Returns the nearest byte length</summary>
 int NearestByte (int digit, bool isLargestNum) {
 	int bitLen = 8;
 	for (int i = 1; i <= digit; i++) {
-		if (bitLen >= digit) return bitLen;
-		bitLen *= 2;                                                        // Bits are of length 8,16 or 32 
+		if (bitLen >= digit) break;
+		bitLen *= 2;                                                   // Bits are of length 8,16 or 32 
 	}
 	if (isLargestNum == true) bitLen *= 2;
 	return bitLen;
@@ -57,25 +58,23 @@ char* BinaryString (bool isNegative, int len, int num) {
 
 /// <summary>Converts the given input to binary</summary>
 char* Binary (int num) {
-	int digit = 0, numCpy = num;
-	int power = 1;
-	bool largestNum = 0, isNegative = 0;                              // Variable largestNum sets if the number is of order (2**(8n)-1)                
+	int numCpy = num, power = 1, digit = 0;
+	bool largestNum = 0, isNegative = 0;                               // Variable largestNum sets if the number is of order (2**(8n)-1)                
 	if (num < 0) {
 		num = -num, numCpy = num, isNegative = 1;
-		long int newNum;
+		int newNum;
 		for (digit = 0; numCpy > 0; digit++) numCpy = numCpy >> 1;
-		if (digit >= 32) return NULL;                                  // Negative number with 32 bit length require 64 bit representation hence omitted
+		if (digit >= 31) return NULL;                                   // Only negative numbers upto 30 bits are evaluated 
 		for (int i = 0; i < digit + 1; i++) power = power << 1;
 		if (num == (power - 1)) {
 			largestNum = 1;
-			newNum = (num ^ ((2 * power) - 1));         // 1's complement using EXOR bitwise operator 
+			newNum = (num ^ ((2 * power) - 1));                          // 1's complement using EXOR bitwise operator 
 		} else newNum = (num ^ (power - 1));
-		newNum++;                                      // 2's complement i.e incrementing 1's complement
+		newNum++;                                                       // 2's complement i.e incrementing 1's complement
 		num = newNum, numCpy = num;
 	}
 	for (digit = 0; numCpy > 0; digit++) numCpy = numCpy >> 1;
 	if (digit > 32) return NULL;
-	if (digit < 0) return NULL;
 	for (int i = 0; i < digit + 1; i++) power = power << 1;
 	if (num == (power - 1)) largestNum = 1;
 	char* bin = BinaryString (isNegative, NearestByte (digit, largestNum), num);
@@ -105,42 +104,40 @@ void Hex (int num) {
 }
 
 /// <summary>Prints the output of the program</summary>
-int Call (int num) {
-	char* bin;
-	bin = Binary (num);
+void Call (int num) {
+	char* bin = Binary (num), * hex = Hexadecimal (bin);
 	if (bin == NULL) {
-		printf ("The input exceeds the maximum length (32 bit)\n");
-		return ERROR_EXCEEDED_LENGTH;
+		printf ("The given negative number is too large(above 30 bits)\n");
+		return;
 	}
-	char* hex = Hexadecimal (bin);
 	printf ("BIN : %s\nHEX : %s\n", bin, hex);
 	free (bin);
 	free (hex);
-	return 0;
 }
 
 int main () {
 	int c, lenIn = 0;
-	char* inStr = malloc (sizeof (char) * 11);
-	if (inStr == NULL) return ERROR_MEM_ALLOC_FAILURE;
+	char inStr[12];
 	printf ("Enter an integer : ");
 	while ((c = getchar ()) != '\n') {
 		if ((lenIn == 0 && c == '-') || (c >= '0' && c <= '9' && lenIn <= 10)) {        // First character can be negative, others within 0-9
 			inStr[lenIn++] = c;
 		} else if (lenIn > 10) {                                                        // Exceeds maximum bit length (32 bit)
-			printf ("The input exceeded maximum length\n");
-			free (inStr);
-			return ERROR_EXCEEDED_LENGTH;
+			printf ("The input exceeded maximum length \nEnter an integer : ");
+			lenIn = 0;
+			while (c != '\n')c = getchar ();
 		} else {
-			printf ("The input is not an integer\n");                       // Returns for non-integer inputs
-			free (inStr);
-			return ERROR_INVALID_INPUT;
+			printf ("The input is not an integer \nEnter an integer : ");                // Prints when a non-integer input is given
+			lenIn = 0;
+			while (c != '\n')c = getchar ();
 		}
 	}
 	inStr[lenIn] = '\0';
-	int num = atoi (inStr);
-	free (inStr);
-	if (num != 0) Call (num);
-	else printf ("BIN : 00000000\nHEX : 0000\n");                         // For input = 0
+	long long int input = atoll (inStr);
+	if (input <= INT_MAX && input >= INT_MIN) {
+		int num = (int)input;
+		if (num == 0) printf ("BIN : 00000000\nHEX : 0000\n");                           // For input = 0
+		else Call (num);
+	} else printf ("The number exceedes 32 bit\n");
 	return 0;
 }
