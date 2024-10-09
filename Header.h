a40566e43566d367a42991e3acf1e46cfb94dfe0
Ayisha <Ayisha.Ahamed@trumpf.com>
2024-10-09 13:58:22 +0530

    A5 : Initial commit

--- Header.h
+++ Header.h
@@ -5,5 +5,24 @@
 // Ayisha Sameera,GET.
 // ------------------------------------------------------------------
 // Header.h
-// Program on branch main.
+// Program on branch A5.
+// Assignment 5 : Implementation of heap sort and binary search
 // ------------------------------------------------------------------------------------------------
+#define RED "\033[1;31m"                   // Color codes
+#define GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define BLUE "\033[1;34m"
+#define MAGENTA "\033[1;35m"
+#define CYAN "\033[1;36m"
+#define RESET "\033[0m"
+
+#define INVALID_INDEX -1
+
+/// <summary>Changes the array to max heap format</summary>
+void Heapify (int arr[], int length, int head);
+
+/// <summary>Recursively deletes the max element in max heap </summary>
+void HeapSort (int arr[], int length);
+
+/// <summary>Finds the index position of an element in the sorted array</summary>
+int BinarySearch (int arr[], int low, int high, int num);
