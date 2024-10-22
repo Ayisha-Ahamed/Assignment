58a3b60c2fea256249c712ce1e57009e9b52b1a0
Ayisha <Ayisha.Ahamed@trumpf.com>
2024-10-22 17:05:29 +0530

    A5 : Binary search added in table

--- Test.c
+++ Test.c
@@ -11,21 +11,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <malloc.h>
-#include <string.h>
 #include <stdlib.h>
-
-typedef struct {
-   int error;            // Stores the error code.
-   int num;              // Stores the input value.
-}Type;
-
-/// <summary>Windows OS specific command to clear console screen.</summary>
-static void ClearScreen () {
-#ifdef _WIN32
-   system ("cls");
-#endif
-   while (getchar () != '\n');
-}
+#include <limits.h>
 
 /// <summary>Checks if the array is sorted in ascending order.</summary>
 bool IsSorted (int arr[], int length) {
@@ -34,27 +21,30 @@ bool IsSorted (int arr[], int length) {
 }
 
 void PrintArray (int arr[], int size, int expLength, char* str) {
-   printf (YELLOW" %s\t"RESET"|", str);
-   for (int i = 0; i < size; i++) printf ("%12d", arr[i]);
+   printf (YELLOW" %s\t\t "RESET"|", str);
+   for (int i = 0; i < size; i++) printf ("%8d", arr[i]);
    for (int i = expLength - size; i > 0; i--) printf ("%12s", "");
 }
 
-void TestHeapSort () {
+void TestSortAndSearch () {
    int arr[8][8] = { { 1,2,3,5,4,6,7,8 } , { 8,7,6,5,4,3,2,1 } ,
                      { -1,-3,-5,-9,-234,-90,-123,0 } ,
                      { 0,5,-567,67,8,2,9,124 } ,
                      { 1,1,1,1,1,1,1,1 } , { 2 } };
-   int size[] = { 8,8,8,8,8,1 };
+   int size[] = { 8,8,8,8,8,1 }, search[] = { 3,8,-9,9,1,2 }, searchRes[] = { 2,7,3,5,0,0 };
    int length = sizeof (size) / sizeof (int);
    for (int i = 0; i < length; i++) {
-      printf ("   \n----------------------------------------------------------------------------------------------------------------------------\n");
+      printf ("   \n---------------------------------------------------------------------------------------\n");
       PrintArray (arr[i], size[i], 8, "Input");
       HeapSort (arr[i], size[i]);
-      printf ("      | %4s", IsSorted (arr[i], size[i]) ? CYAN"   Pass     "RESET"\n" : MAGENTA"   Fail     "RESET"\n");
-      PrintArray (arr[i], size[i], 8, "Output");
-      printf ("      | %4s", "");
+      PrintArray (arr[i], size[i], 8, "\n Output");
+      int index = BinarySearch (arr[i], size[i], search[i]);
+      printf ("\n"YELLOW" Search Input"RESET"  \t |\t %d", search[i]);
+      printf ("\n"YELLOW" Expected Output"RESET" |\t %d\n"YELLOW" Actual Output"RESET" \t |\t %d", searchRes[i], index);
+      printf ("\n"YELLOW" Status"RESET"\t\t |  \t%12s",
+              IsSorted (arr[i], size[i]) && index == searchRes[i] ? CYAN"Pass"RESET : MAGENTA"Fail"RESET);
    }
-   printf ("   \n----------------------------------------------------------------------------------------------------------------------------\n");
+   printf ("\n---------------------------------------------------------------------------------------\n");
 }
 
 /// <summary>Gets user input and returns true if 'y' is pressed.</summary>
@@ -69,71 +59,61 @@ bool Choice () {
    Choice ();
 }
 
-/// <summary>Gets integer from the user and checks if the input is valid.</summary>
-Type GetInt () {
-   char str[150], strCpy[150];
-   fgets (str, sizeof (str), stdin);
-   Type error = { 0, -1 };
-   if (str[0] == '-') strcpy (strCpy, str + 1);
-   else strcpy (strCpy, str);
-   strCpy[strcspn (strCpy, "\n")] = '\0';                                     // Removes the new line character at the end of the input string
-   if (strspn (strCpy, "0123456789") == 0) error.error = 1;                   // Checks if the input has any non-integer character
-   long long int num = atoll (str);
-   if (strlen (str) > 12 || num > INT_MAX || num < INT_MIN) error.error = 2;  // Checks if the input exceeds INT range
-   else  error.num = (int)num;
-   switch (error.error) {
-      case 1: printf (MAGENTA"Invalid input. Please provide a valid integer and press 'enter'\n"RESET); break;
-      case 2: printf (MAGENTA"Input is out of range\n"RESET); break;
+/// <summary>Gets integer from the user.</summary>
+int GetInt (char* input) {
+   while (1) {
+      printf ("%s : ", input);
+      char strInput[15], * endptr = NULL;
+      fgets (strInput, 15, stdin);
+      long long int num = strtoll (strInput, &endptr, 10);               // Converts input string to long long int
+      if (strchr (strInput, '\n') == NULL) while (getchar () != '\n');   // If the string length exceeds 14, clear input buffer
+      if (*endptr != '\n' || num > INT_MAX || num < INT_MIN || strInput[0] == '\n') {
+         printf (MAGENTA"Invalid input. Please provide an integer within INT range and press 'enter' \n"RESET);
+         continue;
+      }
+      return (int)num;
    }
-   return error;
 }
 
 /// <summary>Prints the first index position of input element.</summary>
 void Search (int array[], int length) {
-   printf (BLUE"\n\nEnter an integer to search : "RESET);
-   Type search = GetInt ();
-   if (search.error == 0) {
-      int index = BinarySearch (array, length - 1, search.num);
-      if (index == -1) printf ("The given integer doesn't exist in the array\n");
-      else printf ("The given integer is found at index position %d\n", index);
-   }
+   int search = GetInt (CYAN"\n\nEnter an integer to search"RESET);
+   int index = BinarySearch (array, length - 1, search);
+   if (index == -1) printf ("The given integer doesn't exist in the array\n");
+   else printf ("The given integer is found at index position %d\n", index);
    printf ("Do you wish to search another integer? (y/n) ");
    if (Choice () == false) return;
-   while (getchar () != '\n');               // Clear input buffer
+   while (getchar () != '\n');
    Search (array, length);
 }
 
 /// <summary>Gets user input and implements sort and search.</summary>
 void ManualTest () {
-   ClearScreen ();
-   printf ("Enter the number of integers in the array : ");
-   Type length = GetInt ();
-   if (length.num <= 0 && length.error == 0) printf (MAGENTA"Invalid length. A list should have atleast one element\n"RESET);
-   else if (length.error == 0) {
-      int* array = malloc (sizeof (int) * length.num);
+   system ("cls");
+   while (getchar () != '\n');
+   int length = GetInt (CYAN"Enter the number of integers in the array"RESET);
+   if (length <= 0 || length > 50) printf (MAGENTA"Invalid length. Please enter a value between 1-50 \n"RESET);
+   else {
+      int* array = malloc (sizeof (int) * length);
       if (array == NULL) return;
-      for (int index = 0; index < length.num; index++) {
-         printf ("Enter integer %d : ", index + 1);
-         Type value = GetInt ();
-         if (value.error != 0) {
-            index--;
-            continue;
-         };
-         array[index] = value.num;
+      for (int index = 0; index < length; index++) {
+         char prompt[50];
+         sprintf (prompt, "Element %d", index + 1);     // Converts index to string
+         int value = GetInt (prompt);
+         array[index] = value;
       }
-      PrintArray (array, length.num, length.num, "Input");
-      HeapSort (array, length.num);
-      printf ("\n");
-      PrintArray (array, length.num, length.num, "Output");
-      printf (YELLOW"\n Status | %8s%s"RESET, "", IsSorted (array, length.num) ? CYAN"Pass\n"RESET : MAGENTA"Fail\n"RESET);
-      Search (array, length.num);
+      PrintArray (array, length, length, "\n Input");
+      HeapSort (array, length);
+      PrintArray (array, length, length, "\n Output");
+      printf (YELLOW"\n Status \t | %6s%s"RESET, "", IsSorted (array, length) ? CYAN"Pass\n"RESET : MAGENTA"Fail\n"RESET);
+      Search (array, length);
       free (array);
       while (getchar () != '\n');
    }
 }
 
 void main () {
-   TestHeapSort ();
+   TestSortAndSearch ();
    printf (YELLOW"Do you wish to give input ? (y/n) "RESET);
    if (Choice () == false) return;
    do {
