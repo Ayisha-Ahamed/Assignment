e107cd7e97403d217614eb16fb992cc96dc1adfc
Ayisha <Ayisha.Ahamed@trumpf.com>
2024-12-25 15:53:40 +0530

    A6B: File handling method update

--- FSM6A.c
+++ FSM6A.c
@@ -5,7 +5,7 @@
 // ------------------------------------------------------------------
 // FSM6A.c
 // Program on branch A6-B.
-// Program that implements identification of patterns '0110' and '1101' in a bit sequence.
+// Program identifies patterns '0110' and '1101' in a bit sequence.
 // ------------------------------------------------------------------------------------------------
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -32,49 +32,47 @@ static State NextState (State currentState, int input, int* output) {
       case S2: return input ? S3 : S1;
       case T1: return input ? T2 : S1;
       case T2: return input ? T2 : T3;
-      case T3:
-         if (!input) return S1;
-         *output = 1;
-         return S2;
-      case S3:
-         if (input) return T2;
-         *output = 1;
-         return T3;
+      case T3: *output = input; return input ? S2 : S1;
+      case S3: *output = !input; return input ? T2 : T3;
    }
    return S0;  // Default return to initial state
 }
 
-static void FSM (FILE* fIn, FILE* fOut, char fOutStr[]) {
+static void FSM (char fInStr[], char fOutStr[], FILE* fOut) {
    State currentState = S0;  // Start in initial state
-   int input = getc (fIn), output = 0, count = 0;
+   int output = 0, count = 0, input = fInStr[count];
    while (input == '0' || input == '1') {
       currentState = NextState (currentState, input - '0', &output);
       fOutStr[count++] = output + '0';
-      input = getc (fIn);
+      input = fInStr[count];
    };
    fOutStr[count] = '\0';
    fprintf (fOut, "%s", fOutStr);
 }
-
-int main (int argc, char* argv[]) {
-   if (argc < 3) {
-      printf ("FSM: Usage <input.txt> <output.txt>\n");
-      return -1;
-   }
-   FILE* fIn = fopen (argv[1], "r"), * fOut = fopen (argv[2], "w");
-   if (fIn == NULL || fOut == NULL) {
-      printf ("FSM: Error opening file %s\n", fIn == NULL ? argv[1] : argv[2]);
-      return -1;
-   }
-   // Calculate the size of the file
-   fseek (fIn, 0L, SEEK_END);
-   size_t size = ftell (fIn);
-   // Reset the file pointer to zeroth index
-   fseek (fIn, 0, SEEK_SET);
-   char* fOutStr = malloc (size + 5);
-   FSM (fIn, fOut, fOutStr);
-   free (fOutStr);
-   fclose (fOut);
-   fclose (fIn);
-   return 0;
-}
+//
+//int main (int argc, char* argv[]) {
+//   if (argc < 3) {
+//      printf ("FSM: Usage <input.txt> <output.txt>\n");
+//      return -1;
+//   }
+//   FILE* fIn = fopen (argv[1], "r"), * fOut = fopen (argv[2], "w");
+//   if (fIn == NULL || fOut == NULL) {
+//      printf ("FSM: Error opening file %s\n", fIn == NULL ? argv[1] : argv[2]);
+//      return -1;
+//   }
+//   // Calculate the size of the file
+//   fseek (fIn, 0L, SEEK_END);
+//   size_t size = ftell (fIn);
+//   // Reset the file pointer to zeroth index
+//   fseek (fIn, 0, SEEK_SET);
+//   char* fOutStr = malloc (size + 5), * fInStr = malloc (size + 5);
+//   if (fInStr == NULL || fOutStr == NULL) return -1;
+//   fread (fInStr, 1, size, fIn);
+//   fInStr[size] = '\0';
+//   FSM (fInStr, fOutStr, fOut);
+//   free (fOutStr);
+//   free (fInStr);
+//   fclose (fOut);
+//   fclose (fIn);
+//   return 0;
+//}
--- FSMTest.c
+++ FSMTest.c
@@ -4,7 +4,7 @@
 // Ayisha Sameera,GET.
 // ------------------------------------------------------------------
 // FSMTest.c
-// Program on branch A6B.
+// Program on branch A6-B.
 // Program for testing FSM6A.c.
 // ------------------------------------------------------------------------------------------------
 
@@ -17,12 +17,14 @@
 #define NTESTS 5
 
 static int ExecProgram (char* exeFilePathAndName, char* inputFilePathAndName, char* outputFilePathAndName) {
-   char* cmdline = malloc (strlen (exeFilePathAndName) + strlen (inputFilePathAndName) + strlen (outputFilePathAndName) + 3);
+   char* cmdline = malloc (strlen (exeFilePathAndName) + strlen (inputFilePathAndName) +
+                           strlen (outputFilePathAndName) + 3);
    if (cmdline == NULL) {
       printf ("Unable to allocate memory\n");
       return 1;
    }
-   sprintf (cmdline, "%s%s%s%s%s", exeFilePathAndName, " ", inputFilePathAndName, " ", outputFilePathAndName);
+   sprintf (cmdline, "%s%s%s%s%s", exeFilePathAndName, " ", inputFilePathAndName, " ",
+            outputFilePathAndName);
 
    // Set up structures for process information
    STARTUPINFOA si;
@@ -65,34 +67,50 @@ static int ExecProgram (char* exeFilePathAndName, char* inputFilePathAndName, ch
 }
 
 // Function to compare the output file with the reference file
-static int FileCompare (FILE* ref, FILE* fOut, int* bitNo) {
-   char refChar = getc (ref), outChar = getc (fOut);
+static int FileCompare (FILE* fRef, FILE* fOut, int* bitNo) {
+   fseek (fRef, 0L, SEEK_END);
+   fseek (fOut, 0L, SEEK_END);
+   size_t refSize = ftell (fRef), outSize = ftell (fOut);
+   fseek (fRef, 0, SEEK_SET);
+   fseek (fOut, 0, SEEK_SET);
+   char* fRefStr = malloc (refSize + 5), * fOutStr = malloc (outSize + 5);
+   if (fRefStr == NULL || fOutStr == NULL) return -3;
+   fread (fRefStr, 1, refSize, fRef);
+   fread (fOutStr, 1, refSize, fOut);
+   fRefStr[refSize] = '\0';
+   fOutStr[outSize] = '\0';
    *bitNo = 0;
-   while (refChar != EOF && outChar != EOF) {
+   char refChar = fRefStr[*bitNo], outChar = fOutStr[*bitNo];
+   while (refChar != '\0' && outChar != '\0') {
       *bitNo += 1;
-      if (refChar != outChar) return refChar;  // Return reference character
-      refChar = getc (ref);
-      outChar = getc (fOut);
+      if (refChar != outChar) {
+         free (fRefStr);
+         free (fOutStr);
+         return refChar;
+      }  // Return reference character
+      refChar = fRefStr[*bitNo];
+      outChar = fOutStr[*bitNo];
    }
+   free (fRefStr);
+   free (fOutStr);
    // To check if both the output and the reference files are of same length
    if (refChar != outChar)
-      return outChar == EOF ? -1 : -2;
+      return outChar == '\0' ? -1 : -2;
    return 2; // Successful comparision of files
 }
 
 int main (int argc, char** argv) {
+   char input[MAX_STR], output[MAX_STR] = "Files/temp.txt", ref[MAX_STR];
+   int passCount = 0;
    if (argc != 2) {
       printf ("Usage: %s <FSM executable name>\n,", argv[0]);
       return -1;
    }
    printf ("FSM Test Harness\n");
-   char input[MAX_STR], output[MAX_STR], ref[MAX_STR];
    for (int i = 0; i < NTESTS; i++) {
       sprintf (input, "Files/test%dIn.txt", i + 1);
-      sprintf (output, "Files/test%dOut.txt", i + 1);
       sprintf (ref, "Files/test%dRef.txt", i + 1);
-      if (ExecProgram (argv[1], input, output) != 0)
-         printf ("Error executing test %d\n", i + 1);
+      if (ExecProgram (argv[1], input, output) != 0) printf ("Error executing test %d\n", i + 1);
       else {
          FILE* fRef = fopen (ref, "r"), * fOut = fopen (output, "r");
          if (fRef == NULL || fOut == NULL) {
@@ -103,12 +121,15 @@ int main (int argc, char** argv) {
          switch (result) {
             case -1: case -2: printf ("Error at bit no. %d %s reached EOF\n",
                                       bitNo + 1, result == -1 ? output : ref); break;
+            case -3: printf ("Error allocating memory\n"); break;
             case 2: printf ("No error testing %s\n", input); break;
             default: printf ("Error at bit no. %d, Expected %d, Actual %d\n",
                              bitNo, result - '0', result == '0' ? 1 : 0); break;
          }
+         passCount += result - 1;
          if (result != -1) fclose (fRef);
          if (result != -2) fclose (fOut);
       }
    }
+   printf ("Test %s\n", passCount == 5 ? "Pass" : "Fail");
 }
