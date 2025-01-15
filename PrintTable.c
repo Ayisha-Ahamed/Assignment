0f9a600eabcf2b15a99bd168d219bde060350660
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-01-15 17:28:21 +0530

    AB6: Multiplication tables

--- PrintTable.c
+++ PrintTable.c
@@ -1,9 +1,21 @@
-#pragma once
 // ------------------------------------------------------------------------------------------------
 // Training ~ A training program for new joiners at Metamation, Batch - July 2024.
 // Copyright (c) Metamation India.
 // Ayisha Sameera,GET.
 // ------------------------------------------------------------------
-// Header.h
-// Program on branch main.
+// Test.c
+// Program on branch AB6.
+// Program to print multiplication tables.
 // ------------------------------------------------------------------------------------------------
+#include <stdio.h>
+
+void PrintTable () {
+   for (int i = 1; i < 11; i++, printf ("\n"))
+      for (int j = 1; j < 11; j++)
+         printf ("%2d *  %2d = %-2d\n", i, j, i * j);
+}
+
+int main () {
+   PrintTable ();
+   return 0;
+}
